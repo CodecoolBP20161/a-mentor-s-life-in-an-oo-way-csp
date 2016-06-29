838be93111b886aac9f2bd3bffb88608c609f512
berloc <berloc1987@gmail.com>
2016-06-29 11:22:33 +0200

    started cc_class.py, modified cc_class.md

--- codecool_class.py
+++ codecool_class.py
@@ -3,4 +3,28 @@ from student import Student
 
 
 class CodecoolClass:
-    pass
+    self.mentors = []
+    self.students = []
+    self.location = "Budapest"
+    self.year = "2016"
+    self.events = []
+    self.retrospectives = []
+
+    '''def __init__(self, location, year, mentors, students, retrospectives, events):
+        self.location = location
+        self.year = year
+        self.mentors = mentors
+        self.students = students
+        # self.retrospectives = !!!!!!!!!!!!!!!!!!!!
+        self.events = Event.event_list'''
+
+    @classmethod
+    def generate_local(cls):
+        cls.mentors = Mentor.create_by_cs("/data/mentors.csv")
+        cls.students = Student.create_by_csv("/data/students.csv")
+        cls.events = Event.create_by_csv("/data/events.csv")
+        for student in cls.students:
+            cls.retrospectives.append(student.retrospective_object)
+            
+        # cls.retrospectives =
+        # return CodecoolClass
