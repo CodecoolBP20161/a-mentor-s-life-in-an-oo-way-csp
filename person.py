38983674826fbabf402c5ca5025103336ff57275
makaimark <makaimark@gmail.com>
2016-06-29 14:34:40 +0200

    return value added to the changer functions

--- person.py
+++ person.py
@@ -9,11 +9,20 @@ class Person:
             raise ValueError("Genders: male, female, not sure")
         else:
             self.gender = gender
-        self.energy_level = energy_level
-        self.happiness_level = happiness_level
+        self.energy_level = int(energy_level)
+        self.happiness_level = int(happiness_level)
 
     def gym(self):
         if self.energy_level < 100:
             self.energy_level += 30
             if self.energy_level > 100:
                 self.energy_level = 100
+        return "Gym gym gym, húhhhhh"
+
+    def energy_level_changer(self, delta):
+        self.energy_level += int(delta)
+        return self.energy_level
+
+    def happiness_level_changer(self, delta):
+        self.happiness_level += int(delta)
+        return self.happiness_level
