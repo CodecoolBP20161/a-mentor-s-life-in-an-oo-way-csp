2c1f4d3156f327f3ab07aa62eb3328e451a2e051
makaimark <makaimark@gmail.com>
2016-06-29 13:48:19 +0200

    1.0

--- story.py
+++ story.py
@@ -1,5 +1,35 @@
 from codecool_class import CodecoolClass
-from mentor import Mentor
-from student import Student
 
-codecool_bp = CodecoolClass.create_local
+
+School = CodecoolClass.generate_local()
+print("Welcome to Codecool ", School.location + " " + str(School.year))  # print out the welcome text
+# user_input = input()
+
+print("The number of students", len(School.students))   # print out the students
+print("-"*20)
+for i in School.students:
+    print(i.first_name + " " + i.last_name)
+# user_input = input()
+
+print("\n" + "The number of mentors", len(School.mentors))  # print out the mentors
+print("-"*20)
+for i in School.mentors:
+    print(i.first_name + " " + i.last_name)
+# user_input = input()
+
+energy_level_counter = 0
+for i in School.students:       # We check the enery level of the students
+    energy_level_counter += int(i.energy_level)
+energy_level_counter = energy_level_counter//len(School.students)
+print("-"*20)
+print("\n" + "The average energy level of the class is: " + str(energy_level_counter))
+if energy_level_counter < 70:
+    print(School.mentors[1].first_name + " " + School.mentors[1].last_name, ':"Class energy is low guys, have a gym"')
+    for i in School.students:
+        print(i.gym())
+for i in School.students:       # We check the enery level of the students again
+    energy_level_counter += int(i.energy_level)
+energy_level_counter = energy_level_counter//len(School.students)
+print("\n" + "The average energy level of the class is: " + str(energy_level_counter))
+print(School.mentors[0].first_name + " " + School.mentors[0].last_name, ': "I think that the energy level is optimal"')
+# user_input = input()
