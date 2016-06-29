9a39195961cc2bdef4fb74e92f24e4cde7802388
makaimark <makaimark@gmail.com>
2016-06-29 11:53:28 +0200

    it is working^^

--- codecool_class.py
+++ codecool_class.py
@@ -6,28 +6,24 @@ from education import Education
 
 
 class CodecoolClass:
-    mentors = []
-    students = []
-    location = "Budapest"
-    year = "2016"
-    events = []
-    retrospectives = []
-    education = []
-    '''def __init__(self, location, year, mentors, students, retrospectives, events):
+
+    def __init__(self, location, year, mentors, students, retrospectives, events, educations):
         self.location = location
         self.year = year
         self.mentors = mentors
         self.students = students
-        # self.retrospectives = !!!!!!!!!!!!!!!!!!!!
-        self.events = Event.event_list'''
+        self.retrospectives = retrospectives
+        self.events = events
+        self.educations = educations
 
     @classmethod
     def generate_local(cls):
-        cls.mentors = Mentor.create_by_cs("/data/mentors.csv")
-        cls.students = Student.create_by_csv("/data/students.csv")
-        cls.events = Event.create_by_csv("/data/events.csv")
-        for student in cls.students:
-            cls.retrospectives.append(student.retrospective_object)
-        cls.education = Education.create_by_csv("/data/education.csv")
-
-School = CodecoolClass.generate_local()
+        mentors = Mentor.create_by_csv("data/mentors.csv")
+        students = Student.create_by_csv("data/students.csv")
+        events = Events.create_by_csv("data/events.csv")
+        retrospectives = []
+        for student in students:
+            retrospectives.append(student.retrospective_object)
+        education = Education.create_by_csv("data/education.csv")
+        local_codecool_class = cls("Budapest", 2016, mentors, students, retrospectives, events, education)
+        return local_codecool_class
